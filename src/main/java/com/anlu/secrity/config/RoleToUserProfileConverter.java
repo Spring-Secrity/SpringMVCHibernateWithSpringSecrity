3d2ca399cf81661635704e6e664bc5e2a4ced6be
anlu <357348329@qq.com>
2018-01-07 11:58:30 +0800

    commit

---
+++ src/main/java/com/anlu/secrity/config/RoleToUserProfileConverter.java
@@ -0,0 +1,29 @@
+package com.anlu.secrity.config;
+
+import com.anlu.secrity.model.UserProfile;
+import com.anlu.secrity.service.UserProfileService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class RoleToUserProfileConverter implements Converter<Object, UserProfile> {
+    static final Logger logger = LoggerFactory.getLogger(RoleToUserProfileConverter.class);
+
+    @Autowired
+    UserProfileService userProfileService;
+
+    /**
+     * Gets UserProfile by Id
+     * @see org.springframework.core.convert.converter.Converter#convert(java.lang.Object)
+     */
+    public UserProfile convert(Object element) {
+        Integer id = Integer.parseInt((String)element);
+        UserProfile profile= userProfileService.findById(id);
+        logger.info("Profile : {}",profile);
+        return profile;
+    }
+}
